53b8b718471c6d9ca0f3814627950a9194b0118d
Vraj <vrajpatel00222@gmail.com>
2024-06-26 20:47:56 +0530

    write the overview of tasks

---
+++ index.js
@@ -0,0 +1,8 @@
+// 1. Deposit some money 
+// 2. Determine number of lines to bet on 
+// 3. collect a bet amount 
+// 4. spin the slot machine 
+// 5. check if user won 
+// 6. give the user their winnings 
+// 7. play again 
+ 
